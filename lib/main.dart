3d2a50ab5ecd6106d276384527ecb70ba9658e89
developeratcsrl <132581798+developeratcsrl@users.noreply.github.com>
2025-06-19 19:40:33 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,10 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
       theme: ThemeData(
           primaryColor: kPrimaryColor,
           scaffoldBackgroundColor: Colors.white,
